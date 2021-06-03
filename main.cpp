5af2c1b20daac594d7064477b8ae367b5da2713f
eropka <kushnarenko.03@inbox.ru>
2021-06-03 20:54:54 +0300

    My first commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i, j;
+    cout << "Input i and j:\n";
+    cin >> i >> j;
+    cout << "chisla: " << i << "; " << j;
+    return 0;
+}
